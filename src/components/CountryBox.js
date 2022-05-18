624e4017d67b85c6ed605c0cfb48ddb1ecc90b5c
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-19 00:03:04 +0100

    added flag overlay to box and moved country info to the left

--- src/components/CountryBox.js
+++ src/components/CountryBox.js
@@ -6,30 +6,33 @@ const CountryBox = ({ name, capital, area, population, region, flag }) => {
 
     return (
         <div className="countryBox box">
+            <div className="flagOverlay">
+                <img src={flag} alt={name + ' ' + 'overlay flag'} />
+            </div>
             <div className="flagContainer">
                 <img src={flag} alt={name + ' ' + 'flag'} />
             </div>
             <div className="countryInfo">
-                <h3 className="countryName">
-                    {name}
-                </h3>
                 <div className="countryCapital">
                     <small><Icon className='countryCapitalIcon' icon="map:city-hall" /></small>
                     <small>{capital}</small>
                 </div>
                 <div className="countryArea">
-                    <small>{area && area.toLocaleString()} km&sup2;</small>
                     <small><Icon className='countryAreaIcon' icon="bx:area" /></small>
+                    <small>{area && area.toLocaleString()} km&sup2;</small>
                 </div>
                 <div className="countryPopulation">
                     <small><Icon className='countryPopulationIcon' icon="fluent:people-team-28-filled" /></small>
                     <small>{population.toLocaleString()}</small>
                 </div>
                 <div className="countryRegion">
-                    <small>{region}</small>
                     <small><Icon className='countryRegionIcon' icon="bx:world" /></small>
+                    <small>{region}</small>
                 </div>
             </div>
+            <h3 className="countryName">
+                {name}
+            </h3>
         </div>
     );
 }
