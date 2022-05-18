04a1058a94c07a23695a2a7ef27a19bde9926467
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-18 23:15:58 +0100

    added .env file and added loading text while fetching data

--- src/components/CountriesContainer.js
+++ src/components/CountriesContainer.js
@@ -9,7 +9,7 @@ const CountriesContainer = () => {
 
     const context = useContext(Context)
 
-    const { dispatch, data } = context
+    const { dispatch, data, isLoading } = context
 
     useEffect(() => {
         getCountriesData()
@@ -21,16 +21,19 @@ const CountriesContainer = () => {
             const res = await fetch(`${ process.env.REACT_APP_ENDPOINT }`)
             const data = await res.json()
             dispatch({ type: 'GET_DATA', payload: data })
+            dispatch({ type: 'SET_IS_LOADING', payload: false })
         } catch (error) {
+            dispatch({ type: 'SET_IS_LOADING', payload: false })
             console.log(error);
         }
     }
 
     return (
         <main className="countriesContainer">
-            {data && data.map(country => (
+            {!isLoading && data.map(country => (
                 <CountryBox key={country.name} name={country.name} capital={country.capital} area={country.area} population={country.population} region={country.region} flag={country.flags.svg} />
             ))}
+            {isLoading && <h2 className='loadingText'>Loading...</h2>}
         </main>
     );
 }
