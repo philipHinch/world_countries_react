27f123ee837270f3528edd9eec97196413d02c09
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-05-21 19:15:30 +0100

    removed console log

--- src/components/CountryBox.js
+++ src/components/CountryBox.js
@@ -30,7 +30,6 @@ const CountryBox = ({ name, capital, area, population, region, flag, countryCode
     }
 
     const handleClick = (e) => {
-        console.log(e.target);
         if (e.target.id === countryCode || e.target.parentElement.id === countryCode || e.target.parentElement.parentElement.id === countryCode || e.target.parentElement.parentElement.parentElement.id === countryCode) {
             setShowModal(true)
             //send modal data to context
